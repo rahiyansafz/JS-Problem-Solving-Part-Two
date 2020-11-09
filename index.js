115d3e923a503fe3be3ab6b8a70b5b12a3aea116
rahiyansafz <rahiyansafin@gmail.com>
2020-11-09 13:29:11 +0600

    1 file modified

--- index.js
+++ index.js
@@ -42,10 +42,8 @@ function afunction(x) {
   };
 }
 console.log(afunction("a"));
-
-////
-
-let countryName = [
+///////////////////////////////////////////////////////////
+let countryNameArr = [
   "Bangladesh",
   "Australia",
   "Romania",
@@ -54,11 +52,88 @@ let countryName = [
   "Denmark",
   "Austria"
 ];
-let text = "";
-for (let i = 0; i < countryName.length; i++) {
+
+let newArray = [];
+for (let i = 0; i < countryNameArr.length; i++) {
+  newArray.push(countryNameArr[i].slice(0, 1));
+}
+let b = newArray.toString().replace(/,/g, "");
+console.log(b);
+///////////////////////////////////////////////////////////
+
+let countryName2 = [
+  "Bangladesh",
+  "Australia",
+  "Romania",
+  "Malaysia",
+  "Uganda",
+  "Denmark",
+  "Austria"
+];
+let ola = "";
+for (let i = 0; i < countryName2.length; i++) {
   // newArray.push(countryName[i].slice(0, 1));
-  text += countryName[i].substr(i, 1);
+  ola += countryName2[i].substr(i, 1);
 }
 // let b = newArray.toString().replace(/,/g, "");
 // console.log(b);
-console.log(text);
+console.log(ola);
+
+//////////////////////////////////////////////////
+
+let countryName3 = [
+  "Bangladesh",
+  "Australia",
+  "Romania",
+  "Malaysia",
+  "Uganda",
+  "Denmark",
+  "Austria"
+];
+let textt = "";
+for (let i = 0; i < countryName3.length; i++) {
+  // newArray.push(countryName[i].slice(0, 1));
+  // text += countryName[i].substr(i, 1);
+  if (countryName3[i].substr(i, 1) === "a") {
+    textt += countryName3[i].substr(i, 1);
+  }
+}
+// let b = newArray.toString().replace(/,/g, "");
+// console.log(b);
+console.log(textt);
+/////////////////////
+
+let countryNameLast = [
+  "Bangladesh",
+  "Australia",
+  "Romania",
+  "Malaysia",
+  "Uganda",
+  "Denmark",
+  "Austria"
+];
+let vowels = ["a", "e", "i", "o", "u"];
+let text = "";
+function findingVowels() {
+  for (let i = 0; i < countryNameLast.length; i++) {
+    if (vowels.includes(countryNameLast[i].toLowerCase().substr(i, 1))) {
+      text += countryNameLast[i].substr(i, 1);
+    } else {
+      text += "N";
+    }
+  }
+  return text;
+}
+console.log(findingVowels());
+////////////////////////////
+///////////////////////////
+function newFunc() {
+  let testee = "";
+  for (let a of "Bangladesh") {
+    if (vowels.includes(a)) {
+      testee += a;
+    }
+  }
+  return testee;
+}
+console.log(newFunc());
